619fb21a659e6e73cf3648767fa614e57f5ee926
Stefan Chen <zchen158@my.bcit.ca>
2022-05-22 15:34:21 -0700

    Implement routes for create user and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 // REGISTER 
+
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET).toString()
   });
-
+ 
   try{
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
@@ -17,6 +19,24 @@ router.post('/register', async (req, res) => {
   }
 })
 
+//LOGIN
+
+router.post('/login', async (req, res) => {
+  try{
+    const user = await User.findOne({username: req.body.username});
+    !user && res.status(401).json('User does not exist, please create a new user');
+    
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET);
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    originalPassword != req.body.password && res.status(401).json('Password is incorrect, please try again');
+
+    const {password, ...others} = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 
 module.exports = router;
